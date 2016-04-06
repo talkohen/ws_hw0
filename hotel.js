637df54330f042b277c39c312e1f2bed37cbd738
Tal Kohen <coen.tal@gmail.com>
2016-04-06 17:32:37 +0300

    initial commit
    hotel like counter

---
+++ hotel.js
@@ -0,0 +1,30 @@
+var events = require ('events');
+var util = require ('util');
+
+util.inherits (hotel, events.EventEmitter);
+
+function hotel (name) {
+
+    this.name = name ;
+    this.likeNum =0;
+    this.log = "\n"+this.name +"'s History Log : \n";
+    events.EventEmitter.call (this);
+
+}
+
+hotel.prototype.like = function () {
+    this.likeNum += 1;
+    this.emit ("likeNumChanged");
+}
+
+hotel.prototype.unlike = function () {
+    this.likeNum-=1;
+    this.emit ("likeNumChanged");
+}
+
+hotel.prototype.addToLog = function (entry) {
+     this.log += entry;
+}
+
+
+module.exports = hotel;
---
+++ index.js
@@ -0,0 +1,51 @@
+var hotel = require ('./hotel');
+var http = require ('http')
+
+
+
+function displayLikeNum () {
+    console.log (this.name + "'s likes : " + this.likeNum);
+    this.addToLog("likeNum = " + this.likeNum + "\n");
+}
+
+function checkNegative () {
+    if (this.likeNum < 0) {
+        console.log ("Cant have negative like number ! (" 
+                        +this.likeNum +") setting to 0");
+        this.like();
+        this.addToLog ("Tried to make likeNum negative."
+                        +"fixed with like.\n")
+    }
+}
+
+function addToLog (entry) {
+ this.log += entry;
+}
+var myHotel = new hotel("Sheraton");
+
+myHotel.on ("likeNumChanged", displayLikeNum );
+myHotel.on ("likeNumChanged", checkNegative );
+
+console.log ("\nHello. Testing functions ...\n");
+
+myHotel.like ();
+myHotel.unlike ();
+myHotel.unlike();
+myHotel.like();
+ console.log (myHotel.log);
+
+ console.log ("\nTesting Went OK. continuing to creating server...")
+
+http.createServer (function(req,res) {
+    res.writeHead(200);
+    res.write ("<h1>Hello</h1>" 
+                + myHotel.name+ "'s like number : "
+                 + myHotel.likeNum
+                 );
+    
+    res.end();
+    
+}).listen(8080);
+
+
+console.log ("listening on 8080...");
